2dd7a66da1ed8c6ad830579b4c1f901384e4b2f8
= <=>
2021-11-24 21:15:38 -0400

    addin code

--- TestProjectISBN2/UnitTest1.cs
+++ TestProjectISBN2/UnitTest1.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
 
 namespace TestProjectISBN2
 {
@@ -8,6 +10,12 @@ namespace TestProjectISBN2
         [TestMethod]
         public void TestMethod1()
         {
+            int[] ISBN = new int[] { 9,7,8,0,7,1,6,7,0,3,4,4,0};
+
+            string resultado = ClassISBN2.VerifyISBN(ISBN);
+
+            Assert.AreEqual("ISBN VALIDO", resultado);
+
         }
     }
 }
